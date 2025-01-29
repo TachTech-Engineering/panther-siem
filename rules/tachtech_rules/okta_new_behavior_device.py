942ba4dc898cc7c61259c3e38bfb94437ae71929
Kyle Jerome Thompson <143838246+kylejeromethompson@users.noreply.github.com>
2025-01-29 14:41:59 -0800

    Update okta_new_behavior_device.py

--- rules/tachtech_rules/okta_new_behavior_device.py
+++ rules/tachtech_rules/okta_new_behavior_device.py
@@ -8,19 +8,15 @@ def rule(event):
     if event.get("eventtype") != "policy.evaluate_sign_on":
         return False
 
-    if "Okta Admin Console" not in event.deep_walk("target", "displayName", default=""):
-        return False
-
     behaviors = event.deep_get("debugContext", "debugData", "behaviors")
     if behaviors:
-        return "New Device=POSITIVE" in behaviors and "New IP=POSITIVE" in behaviors
+        return "New Device=POSITIVE" in behaviors
 
     log_only_security_data = event.deep_get("debugContext", "debugData", "logOnlySecurityData")
     if isinstance(log_only_security_data, str):
         log_only_security_data = json.loads(log_only_security_data)
     return (
         deep_get(log_only_security_data, "behaviors", "New Device") == "POSITIVE"
-        and deep_get(log_only_security_data, "behaviors", "New IP") == "POSITIVE"
     )
 
 
@@ -29,7 +25,6 @@ def title(event):
         f"{event.deep_get('actor', 'displayName', default='<displayName-not-found>')} "
         f"<{event.deep_get('actor', 'alternateId', default='alternateId-not-found')}> "
         f"accessed Okta Admin Console using new behaviors: "
-        f"New IP: {event.deep_get('client', 'ipAddress', default='<ipAddress-not-found>')} "
         f"New Device: {event.deep_get('device', 'name', default='<deviceName-not-found>')}"
     )
 
